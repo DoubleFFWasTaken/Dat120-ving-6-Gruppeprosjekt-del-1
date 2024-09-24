f227961ddc9cc28426662debd6a57ef1d72971a4
Filip F <filipfurenes@gmail.com>
2024-09-24 15:02:44 +0200

    nye filer
    jeg liker ikke læreren

---
+++ access files.py
@@ -0,0 +1,10 @@
+#access files
+
+data_sola = open(r"temperatur_trykk_met_samme_rune_time_datasett.csv.txt", "r")
+print(data_sola.read())
+
+
+
+
+
+data_sola.close()
--- plotting.py
+++ plotting.py
@@ -4,8 +4,8 @@ import csv
 
 
 data_sola = open("temperatur_trykk_met_samme_rune_time_datasett.csv", "r")
-#hvorfor valgte han å ha så ekstremt langt filnavn?
-
+#Hvilken psykopat velger å kalle vilen for temperatur_trykk_met_samme_rune_time_datasett.csv.txt
+#JEG HAR LETT SÅ LENGE ETTER EN FEIL SÅ FINNER JEG UT AT DET EN EN .TXT FIL MED NAVNET .CSV HVORFOR FAEN
 
 
 plt.legend()
