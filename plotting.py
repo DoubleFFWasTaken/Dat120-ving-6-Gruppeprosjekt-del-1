4788989e97bcb5c7ed093a581360ff7d76a2611f
Filip F <filipfurenes@gmail.com>
2024-09-24 12:02:58 +0200

    a

--- plotting.py
+++ plotting.py
@@ -1,11 +1,10 @@
 #plotting
 from matplotlib import pyplot as plt
+import csv
 
 
-f = open("temperatur_trykk_met_samme_rune_time_datasett.csv", "r")
+data_sola = open("temperatur_trykk_met_samme_rune_time_datasett.csv", "r")
 #hvorfor valgte han å ha så ekstremt langt filnavn?
-for x in f:
-    0
 
 
 
