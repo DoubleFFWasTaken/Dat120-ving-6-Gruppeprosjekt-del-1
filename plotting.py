3db9d0df858bb882708d4ac433d50f4bd7193aa6
Filip F <filipfurenes@gmail.com>
2024-09-24 11:49:14 +0200

    Dat120 Oblig 6 gruppe 15

---
+++ plotting.py
@@ -0,0 +1,13 @@
+#plotting
+from matplotlib import pyplot as plt
+
+
+f = open("temperatur_trykk_met_samme_rune_time_datasett.csv", "r")
+#hvorfor valgte han å ha så ekstremt langt filnavn?
+for x in f:
+    0
+
+
+
+plt.legend()
+plt.grid()
