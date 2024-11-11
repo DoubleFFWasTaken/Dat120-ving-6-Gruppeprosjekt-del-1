52575a64ba76ea27b3f72dc769fede061a98915d
Filip F <filipfurenes@gmail.com>
2024-11-11 15:39:41 +0100

    Update Data.py

--- Funksjoner/Data.py
+++ Funksjoner/Data.py
@@ -44,67 +44,26 @@ class Data():
                     if self.filnavn == "Filer/trykk_og_temperaturlogg_rune_time.csv.txt":
                     
                         if "am" not in row:
-                            return 0
+                            time_str = row[0]
+                            base_time = dt.datetime.strptime(time_str,"%m.%d.%Y %H:%M")
+                            self.RuneTid.append(base_time)
+                            self.RuneTemp.append(row[4])
                         
                         if "am" in row:
-                            return 0
+                            time_str = row[0]
+                            base_time = dt.datetime.strptime(time_str,"%m/%d/%Y %I:%M:%S %p")
+                            self.RuneTemp.append(row[4])
+                        else: 
+                            print(f"feil i linje {i}, {row[0]}")   
                         
 
                 except Exception as e:
                     print(f"Error processing row {i}: {e}")
 
-            """
-         
-            time_str = f"{row[2]}.{row[1]}.{row[0]}"
-            base_time = dt.datetime.strptime(time_str, "%d.%m.%Y")
-            self.datetime.append(base_time)
-               
-            """
 
-#endre disse til å passe listene fra tidligere, pass på at datoene og listene skal være like, vurderer å bruke arrays eller dictionaries
 
-    def år_max(self):
-        if not hasattr(self, 'årlig_maks'):
-            self.yearly_avg()
-        return self.årlig_maks
+#endre disse til å passe listene fra tidligere
 
-    def år_min(self):
-        if not hasattr(self, 'årlig_min'):
-            self.yearly_avg()
-        return self.årlig_min
-
-    def år_gj(self):
-        if not hasattr(self, 'årlig_gj'):
-            self.yearly_avg()
-        return self.årlig_gj
-
-    def Data_år(self):
-        return self.år
 
     def Data_1(self):
-        return self.måned
-
-    def Data_2(self):
-        return self.dag
-
-    def Data_3(self):
-        return self.rad3
-
-    def Maksimum(self):
-        return self.solflekker
-
-    def Minimum(self):
-        return self.solflekker
-
-    def Data_6(self):
-        return self.rad6
-
-    def Data_7(self):
-        return self.rad7
-    
-    def Gjennomsnitt(self):
-        return self.gj
-
-    def absTime(self):
-        return self.datetime
-    
+        return self.liste
