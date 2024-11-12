0025ee2260f53929a7d6574ebd1ac164d55215b0
Filip F <filipfurenes@gmail.com>
2024-11-12 16:52:51 +0100

    a

--- Funksjoner/plot.py
+++ Funksjoner/plot.py
@@ -2,25 +2,25 @@ from Data import Data
 from matplotlib import pyplot as plt
 
 rune_instance    = Data("Filer/trykk_og_temperaturlogg_rune_time.csv.txt")
-runetemp         = rune_instance.Runetemp()  
-runetrykk        = rune_instance.Runetrykk()
-runetid          = rune_instance.Runetid()
+runetemp         = rune_instance.Runetemp        ()  
+runetrykk        = rune_instance.Runetrykk       ()
+runetid          = rune_instance.Runetid         ()
 
 
 sirdal_instance  = Data("Filer/temperatur_trykk_sauda_sinnes_samme_tidsperiode.csv.txt")
-sinnestemp       = sirdal_instance.Sinnestemp()
-sinnestrykk      = sirdal_instance.Sinnestrykk()
-sinnestid        = sirdal_instance.Sinnestid()  
+sinnestemp       = sirdal_instance.Sinnestemp    ()
+sinnestrykk      = sirdal_instance.Sinnestrykk   ()
+sinnestid        = sirdal_instance.Sinnestid     ()  
 
-saudatemp        = sirdal_instance.Saudatemp()
-saudatrykk       = sirdal_instance.Saudatrykk()
-saudatid         = sirdal_instance.Saudatid()
+saudatemp        = sirdal_instance.Saudatemp     ()
+saudatrykk       = sirdal_instance.Saudatrykk    ()
+saudatid         = sirdal_instance.Saudatid      ()
 
 
 sola_instance    = Data("Filer/temperatur_trykk_met_samme_rune_time_datasett.csv.txt")
-solatemparatur   = sola_instance.Solatemp()
-solatrykk        = sola_instance.SolaTrykk()
-solatid          = sola_instance.Solatid()
+solatemparatur   = sola_instance.Solatemp        ()
+solatrykk        = sola_instance.SolaTrykk       ()
+solatid          = sola_instance.Solatid         ()
 
 
 
@@ -28,9 +28,9 @@ plt.plot(runetid  , runetemp        , label = "runetemp")
 plt.plot(sinnestid, sinnestemp      , label = "sinnestemp")
 plt.plot(saudatid , saudatemp       , label = "saudatemp")
 plt.plot(solatid  , solatemparatur  , label = "solatemp")
-plt.grid()
-plt.legend()
-plt.show()
+plt.grid    ()
+plt.legend  ()
+plt.show    ()
 
 
 #print(runetid)
